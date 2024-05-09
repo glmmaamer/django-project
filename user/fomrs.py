4a90603c78220c16611591c055ef53a3a28c3692
glmmaamer <atackerglm12@gmail.com>
2024-05-09 10:53:15 +0100

    add froms.py  and register.html

--- user/fomrs.py
+++ user/fomrs.py
@@ -2,6 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 
 class create_account(forms.ModelForm):
+    username   = forms.CharField(label='إسم المستخدم',max_length=30)
+    email      = forms.CharField(label='إيمايل')
+    first_name = forms.CharField(label='اللقب')
+    last_name  = forms.CharField(label='الاسم')
+    password1  = forms.CharField(label='كلمة السر',widget=forms.PasswordInput(),min_length=8)
+    password2  = forms.CharField(label='تأكد كلمة السر',widget=forms.PasswordInput(),min_length=8)
+
+
     class Meta:
         model = User
-        fields = ('username','email', 'first_name', 'last_name','password')
+        fields = ('username','email', 'first_name', 'last_name','password1','password2')
