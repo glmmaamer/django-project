09ef77f1d711518be87c68d78775396428326ea3
glmmaamer <atackerglm12@gmail.com>
2024-05-09 15:22:42 +0100

    now create app user add file forms.py

--- user/fomrs.py
+++ user/fomrs.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 
+
+#create accounte 
 class create_account(forms.ModelForm):
     username   = forms.CharField(label='إسم المستخدم',max_length=30)
     email      = forms.CharField(label='إيمايل')
@@ -13,3 +15,18 @@ class create_account(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username','email', 'first_name', 'last_name','password1','password2')
+
+
+    #comparison two password 
+    def comp_pass(self):
+        c_data = self.cleaned_data
+        if c_data['password1'] != c_data['password2']:
+            raise forms.ValidationError('كلمة السر غير متطابقة')
+        return c_data['password2']
+    #comparison username
+    def comp_username(self):
+        c_user = self.cleaned_data
+        if User.objects.filter(username=c_user['username']):
+            raise forms.ValidationError('اسم المستخدم موجود')
+        return c_user['username']
+
