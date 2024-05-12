1a979fe55fc2bdf0d5a5f44f7af9ba85ad21f3ce
glmmaamer <atackerglm12@gmail.com>
2024-05-12 01:07:36 +0100

    create logout page

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('register/',views.register, name='register'),
     path('login/',LoginView.as_view(template_name='user/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
