237c5e2123e8742c3030c8af16a4495f57aaa50b
glmmaamer <atackerglm12@gmail.com>
2024-05-14 00:59:09 +0100

    add function img size 300 x 300

--- user/models.py
+++ user/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from PIL import Image
 
 # Create your models here.
 
 class Profiles(models.Model):
-    img = models.ImageField(default='default.jpg',upload_to='profile_picture')
+    image = models.ImageField(default='default.jpg',upload_to='profile_picture')
     user = models.OneToOneField(User,on_delete=models.CASCADE)
 
 
     def __str__(self):
         return '{}profile'.format(self.user.username)
+    
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.width >300 or img.height >300:
+            size_img = (300,300)
+            img.thumbnail(size_img)
+            img.save(self.image.path)
+        
+        
 
 def create_img(sender, **kwarg):
     if kwarg['created']:
